1a2ce9eb73c2aaa15ebc6c48d1ce232be03288cb
sdetrais <raisjaliatpm@gmail.com>
2026-08-09 22:53:01 +0530

    OOPS Lesson 1 - Constructor, Getter, Setter and ToString() Implementation

---
+++ JavaMastery/OOPS1/OOPS_1/OOPS1.java
@@ -0,0 +1,48 @@
+class Alpha{
+    private String Name;
+    private int rollno;
+    private int marks;
+    private String Grade;
+    
+    public Alpha(String name, int rollno, int marks, String grade) 
+    {
+        this.Name = name;
+        this.rollno = rollno;
+        this.marks = marks;
+        this.Grade = grade;
+    }
+
+    public String getName() {
+        return Name;
+    }
+
+    public int getRollno() {
+        return rollno;
+    }
+
+    public int getMarks() {
+        return marks;
+    }
+
+    public String getGrade() {
+        return Grade;
+    }
+
+    @Override
+    public String toString() {
+        return "Alpha [Name=" + Name + ", rollno=" + rollno + ", marks=" + marks + ", Grade=" + Grade + "]";
+    }
+
+
+}
+
+
+public class OOPS1 {
+
+    public static void main(String[] args) {
+        System.out.println("OOPS Lesson 1 - Constructor, Getter, Setter and ToString() Implementation");
+        Alpha a = new Alpha("Rais", 30, 90, "A");
+        System.out.println(a);
+        
+    }
+}
